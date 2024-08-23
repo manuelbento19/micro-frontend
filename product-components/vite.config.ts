9aea14979d16635ac7c4204d5aaf796257a2f304
Manuel Bento <manuelbentomb.223@hotmail.com>
2024-08-23 14:11:11 +0100

    feat: Serve component list

--- product-components/vite.config.ts
+++ product-components/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "product-components",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./list": "./src/productList"
+      },
+      shared: ["react","react-dom"]
+    })
+  ],
+  build: {
+    modulePreload: true,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false
+  }
 })
